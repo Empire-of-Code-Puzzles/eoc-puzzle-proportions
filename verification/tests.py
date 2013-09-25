86def4c8a7e9f3496de2623e22b638ad769a7779
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-09-25 23:05:26 +1200

    add explanation in tests

--- verification/tests.py
+++ verification/tests.py
@@ -10,44 +10,31 @@ Each test is dict with
 TESTS = {
     "Basics": [
         {
-            "input": {
-                'gold-tin': [1, 2],
-                'gold-iron': [1, 3],
-                'gold-copper': [1, 4],
-            },
-            "answer": [1, 24]
+            "input": {'gold-iron': [1, 3], 'gold-tin': [1, 2], 'gold-copper': [1, 4]},
+            "answer": [1, 24],
+            "explanation": {'iron': [7, 24], 'tin': [11, 24], 'copper': [5, 24], 'gold': [1, 24]}
         },
         {
-            "input": {
-                'tin-iron': (1, 2),
-                'iron-copper': (1, 2),
-                'copper-tin': (1, 2),
-            },
+            "input": {'iron-copper': [1, 2], 'tin-iron': [1, 2], 'copper-tin': [1, 2]},
             "answer": [1, 4],
+            "explanation": {'iron': [1, 4], 'tin': [1, 4], 'copper': [1, 4], 'gold': [1, 4]}
         },
         {
-            "input": {
-                'iron-tin': (2, 3),
-                'iron-copper': (1, 4),
-                'iron-gold': (1, 2)
-            },
-            "answer": [7, 24]
+            "input": {'iron-tin': [2, 3], 'iron-gold': [1, 2], 'iron-copper': [1, 4]},
+            "answer": [7, 24],
+            "explanation": {'iron': [5, 24], 'tin': [11, 24], 'copper': [1, 24], 'gold': [7, 24]}
         },
         {
-            "input": {
-                'gold-copper': (2, 7),
-                'tin-gold': (1, 2),
-                'copper-tin': (5, 9)
-            },
-            "answer": [29, 252]}
+            "input": {'copper-tin': [5, 9], 'gold-copper': [2, 7], 'tin-gold': [1, 2]},
+            "answer": [29, 252],
+            "explanation": {'iron': [83, 252], 'tin': [97, 252], 'copper': [43, 252], 'gold': [29, 252]}
+        }
     ],
     "Extra": [
         {
-            "input": {
-                'gold-tin': (6, 19),
-                'tin-copper': (22, 37),
-                'iron-tin': (2, 7)
-            },
-            "answer": [2143, 9842]}
+            "input": {'tin-copper': [22, 37], 'iron-tin': [2, 7], 'gold-tin': [6, 19]},
+            "answer": [2143, 9842],
+            "explanation": {'iron': [1847, 9842], 'tin': [965, 9842], 'copper': [4887, 9842], 'gold': [2143, 9842]}
+        }
     ]
 }
