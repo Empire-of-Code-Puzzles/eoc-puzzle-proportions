fd8d98868cc32ee9facf2ec7ed50dd49de7a01f8
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-09-26 12:27:44 +1200

    Animation

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -76,16 +76,10 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
                 $content.find('.call').html('Pass: checkio(' + strInput + ')');
                 $content.find('.answer').remove();
             }
-            //Dont change the code before it
 
-            //Your code here about test explanation animation
-            //$content.find(".explanation").html("Something text for example");
-            //
-            //
-            //
-            //
-            //
 
+            var canvas = new AlloysCanvas($content.find(".explanation")[0]);
+            canvas.createCanvas(checkioInput, explanation);
 
             this_e.setAnimationHeight($content.height() + 60);
 
@@ -109,10 +103,68 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
         var colorGrey1 = "#EBEDED";
 
         var colorWhite = "#FFFFFF";
-        //Your Additional functions or objects inside scope
-        //
-        //
-        //
+
+
+        function AlloysCanvas(dom) {
+            var x0 = 10,
+                x1 = 10,
+                x2 = 100,
+                y0 = 10;
+
+            var r = 150;
+            var cell = 30;
+            var padding = 10;
+
+            var fullSizeX = x0 * 2 + r * 2;
+            var fullSizeY = y0 * 2 + r * 2 + cell * 3 + padding * 3;
+
+            var paper = Raphael(dom, fullSizeX, fullSizeY, 0, 0);
+
+            var attrs = {
+                'gold': {"stroke": colorGrey4, "stroke-width": 2, "fill": colorOrange4},
+                'tin': {"stroke": colorGrey4, "stroke-width": 2, "fill": colorBlue1},
+                'copper': {"stroke": colorGrey4, "stroke-width": 2, "fill": colorOrange1},
+                'iron': {"stroke": colorGrey4, "stroke-width": 2, "fill": colorBlue3}
+            };
+            var attrText = {"stroke": colorBlue4, "font-size": cell * 0.6, "font-family": "Verdana"};
+
+            var metals = ['gold', 'tin', 'copper', 'iron'];
+
+            this.createCanvas = function(alloys, partitions) {
+                var angle1 = 0,
+                    angle2 = 0;
+                var center = [fullSizeX / 2, y0 + r];
+                for (var i = 0; i < 4; i++){
+                    var p = partitions[metals[i]][0] / partitions[metals[i]][1];
+                    angle2 = angle1 + Math.PI * 2 * p;
+                    paper.path(Raphael.format(
+                        "M{0},{1}L{2},{3}A{6},{6},{7},0,1,{4},{5}Z",
+                        center[0],
+                        center[1],
+                        center[0] + Math.sin(angle1) * r,
+                        center[1] - Math.cos(angle1) * r,
+                        center[0] + Math.sin(angle2) * r,
+                        center[1] - Math.cos(angle2) * r,
+                        r,
+                        angle2 - angle1
+                    )).attr(attrs[metals[i]]);
+                    angle1 = angle2;
+                }
+                var y1 = y0 + r * 2 + padding;
+                var xText = x1 + cell * 2 + padding * 2 + x2;
+                i = 0;
+                for (al in alloys) {
+                    var ms = al.split("-");
+                    paper.rect(x1, y1 + (padding + cell) * i, cell, cell).attr(attrs[ms[0]]);
+                    paper.rect(x1 + cell + padding, y1 + (padding + cell) * i, cell, cell).attr(attrs[ms[1]]);
+                    paper.text(xText, y1 + (padding + cell) * i + cell / 2,
+                        ms[0] + " + " + ms[1] + " = " + alloys[al][0] + "/" + alloys[al][1]
+                        ).attr(attrText);
+                    i++;
+                }
+            }
+
+        }
 
 
     }
